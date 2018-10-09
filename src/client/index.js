cdcba8684fa70c014b30f556913315c69578d6ee
Rebecca Turk <rebeccaturk@Rebeccas-MBP-2.home>
2018-10-09 14:43:47 -0400

    functional modal

--- src/client/index.js
+++ src/client/index.js
@@ -2,27 +2,19 @@ let request = new XMLHttpRequest();
 
 request.open("GET", "https://newsapi.org/v2/everything?sources=national-geographic&apiKey=58314004c9b44065968db4b96c22f504");
 
-// https://newsapi.org/v2/everything?q=bitcoin&apiKey=58314004c9b44065968db4b96c22f504
-
-// https://newsapi.org/v2/everything?sources=national-geographic&apiKey=58314004c9b44065968db4b96c22f504
-
-// http://www.splashbase.co/api/v1/images/search
-
-// http://www.splashbase.co/api/v1/images/latest
-
 request.onload = function () {
-    let rootElem = document.getElementById("root");
     let modalElem;
+    let rootElem = document.getElementById("root");
     let modal = document.getElementById("myModal");
-    let span = document.getElementsByClassName("close")[0];
-    // let currImage;
+    let closeButton = document.getElementsByClassName("close")[0];
+    let modalInner = document.getElementById("modal-inner");
+    let mountImageHere = document.getElementById("mount-image-here");
+
     let data = this.response;
     let start = 0;
     let finish = 10;
-    // console.log('data: ', data);
     let json = JSON.parse(data);
 
-    // default:
     modal.style.display = "none";
 
     const mountImages = () => {
@@ -36,43 +28,43 @@ request.onload = function () {
                 rootElem.appendChild(currImage);
                 currImage.addEventListener("dblclick", () => {
                     modal.style.display = "block";
-                    modalElem = document.createElement("img");
+                    modalElem = new Image(800);
                     modalElem.id = "modal" + i;
                     modalElem.src = currUrl;
-                    modal.appendChild(modalElem);
+                    mountImageHere.appendChild(modalElem);
                 });
             }
         }
-    }
+    };
 
-    span.onclick = () => {
-        modal.style.display = "block";
-    }
+    closeButton.onclick = () => {
+        mountImageHere.removeChild(modalElem);
+        modal.style.display = "none";
+    };
 
-    // look into this some more:
     window.onclick = (event) => {
-        if(event.target !== modalElem) {
-            modal.removeChild(modalElem);
+        if (event.target == modal) {
+            mountImageHere.removeChild(modalElem);
             modal.style.display = "none";
         }
-    }
+    };
 
     const initialMount = () => {
         mountImages();
-    }
+    };
 
     const getImagesButton = document.getElementById("get-images");
 
     getImagesButton.onclick = () => {
         initialMount();
-    }
+    };
 
     const nextButton = document.getElementById("next-button");
 
     const incrementSliceIndexes = () => {
         start += 10;
         finish += 10;
-    }
+    };
 
     nextButton.onclick = () => {
         const dismountImages = () => {
@@ -83,24 +75,8 @@ request.onload = function () {
         }
         dismountImages();
         incrementSliceIndexes();
-        // console.log('start: ', start);
-        // console.log('finish: ', finish);
         mountImages();
-    }
-}
+    };
+};
   
 request.send();
-
-// FROM USING SPLASHBASE:
-// mountImages();
-
-    // const mountImages = () => {
-    //     for (let i = start; i < finish; i++) {
-    //         let currentUrl = json.images[i].url;
-    //         let currImage = new Image(100);
-    //         currImage.src = currentUrl;
-    //         currImage.id = "image" + i;
-    //         console.log("currImage.id: ", currImage.id);
-    //         rootElem.appendChild(currImage);
-    //     }
-    // }
